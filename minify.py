d944c47c0fe1cedb3e270100de12100fd03ac4de
Mohamed Adel <mosoliman33@gmail.com>
2022-12-14 05:00:02 +0200

    Updated to work with GUI

--- minify.py
+++ minify.py
@@ -1,12 +1,14 @@
-def ClearSpaces(path):
+# from Prettify import filepath
+def Minify(path):
     with open(path,"r") as file:
         lines = file.readlines()
         for i,line in enumerate(lines):
-            trimmedline = line.strip() #trim every line of spaces and place it in the list of lines
+            trimmedline = line.strip()
             if trimmedline.find("\n"):
                 trimmedline.replace("\n","")
             lines[i] = trimmedline
-    with open("C:\\TestCode\\Project CPP\\minified.xml","w") as file:
-        #file.write('\n'.join(lines) + '\n')
-        file.writelines(lines)
-ClearSpaces(filepath)
+    return lines
+    # with open(".\\minified.xml","w") as file:
+    #     #file.write('\n'.join(lines) + '\n')
+    #     file.writelines(lines)
+# Minify(filepath)
