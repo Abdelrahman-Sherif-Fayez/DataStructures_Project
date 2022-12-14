569175ae7e3b03d70ac4cff6b8245ce123572ea2
Mohamed Adel <mosoliman33@gmail.com>
2022-12-14 06:21:53 +0200

    Updated

--- PrettifyWIP.py
+++ PrettifyWIP.py
@@ -1,46 +1,53 @@
 # filepath = "C:\\TestCode\\Project CPP\\sample.xml"
 
+
 def clearSpacing(path):
-    with open(path,"r") as file:
+    with open(path, "r") as file:
         lines = file.readlines()
-        xmlheader = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>'
-        lines.insert(0,xmlheader)
-        for i,line in enumerate(lines):
+        for i, line in enumerate(lines):
             trimmedline = line.lstrip()
             lines[i] = trimmedline
     return lines
 
-def Prettifyfunction(cleared):
+
+def Prettifyfunction(filelines):
     level = 0
     previousstatus = spaced = ""
-    for x,line in enumerate(cleared):
-        openingb = line.find('<') #find positions of tags if exists, otherwise return -1
+    for x, line in enumerate(filelines):
+        # find positions of tags if exists, otherwise return -1
+        openingb = line.find('<')
         closingb = line.find('>')
         closingtag = line.find('/')
         if x == 0:
-            previousstatus = "openedlevel" #to indent in case of header (taking level 0)
-        if openingb !=-1 and closingb !=-1: #check for o/c tags
-            if closingtag != -1 and line[closingtag -1] == '<': #/ is found and before it, the openedleveling tag
-                if line[openingb + 1] != '/' and line[closingtag +1] !='>':
-                    if previousstatus == "openedlevel": #the parent tag didn't close in on the child so child is indented
+            # to indent in case of header (taking level 0)
+            previousstatus = "openedlevel"
+        if openingb != -1 and closingb != -1:  # check for o/c tags
+            # / is found and before it, the openedleveling tag
+            if closingtag != -1 and line[closingtag - 1] == '<':
+                if line[openingb + 1] != '/' and line[closingtag + 1] != '>':
+                    if previousstatus == "openedlevel":  # the parent tag didn't close in on the child so child is indented
                         level += 4
                     previousstatus = "closedlevel"
                 else:
-                    if previousstatus == "closedlevel": #to reset the spacing
+                    if previousstatus == "closedlevel":  # to reset the spacing
                         level -= 4
                     previousstatus = "closedlevel"
-            if closingtag == -1: #keep indenting if there's no parent closing
+            if closingtag == -1:  # keep indenting if there's no parent closing
                 if previousstatus == "openedlevel":
                     level += 4
                 previousstatus = "openedlevel"
-            if closingtag != -1 and line[closingtag - 1] != '<' and line[closingtag +1] !='>':
+            if closingtag != -1 and line[closingtag - 1] != '<' and line[closingtag + 1] != '>':
                 if previousstatus == "openedlevel":
-                    level+=4
+                    level += 4
                 previousstatus = "openedlevel"
         for i in range(level):
             spaced += ' '
         spaced += line
+    spaced = spaced.split("\n")
+    spaced.insert(
+        0, '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>')
+    spaced = '\n'.join(spaced)
     return spaced
+
 # clearSpacing(filepath)
 # Prettifyfunction(filepath)
-
--- minify.py
+++ minify.py
@@ -1,12 +1,15 @@
 # from Prettify import filepath
 def Minify(path):
-    with open(path,"r") as file:
+    with open(path, "r") as file:
         lines = file.readlines()
-        for i,line in enumerate(lines):
+        lines.insert(
+            0, '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>')
+        for i, line in enumerate(lines):
             trimmedline = line.strip()
             if trimmedline.find("\n"):
-                trimmedline.replace("\n","")
+                trimmedline.replace("\n", "")
             lines[i] = trimmedline
+    lines = ''.join(lines)
     return lines
     # with open(".\\minified.xml","w") as file:
     #     #file.write('\n'.join(lines) + '\n')
