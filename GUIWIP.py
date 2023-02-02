de391bb5e665344efe32da5f8ddd7d4715e545b4
Abdelrahman-Sherif-Fayez <bedo.sherif@hotmail.com>
2023-02-03 01:08:55 +0200

    update GUI for json to work

--- GUIWIP.py
+++ GUIWIP.py
@@ -17,7 +17,7 @@ from minify import Minify
 from PyQt5.QtGui import QColor, QRegExpValidator, QSyntaxHighlighter, QTextCharFormat, QTextCursor
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtCore import QRegExp
-from to_JSON import to_Json
+from Completion_of_JSON import Json
 from PyQt5.QtWidgets import *
 
 
